f275675ce8931f70eff56c563f95c41e7b8d0bdb
hhvergara <hhvservice@gmail.com>
2020-12-22 09:32:39 -0300

    tagueamos para probar

--- update.py
+++ update.py
@@ -8,7 +8,15 @@ logging.basicConfig(level=logging.INFO)
 GIT_PYTHON_TRACE = 'full'
 
 repo = git.Repo(git_path)
-print (f'resultado del update: {repo.remotes.origin.pull("")}')
+repo.remotes.origin.pull("")
+
+hcommit = repo.head.commit
+hcommit.diff(None)              # diff tree against working tree
+# Traverse added Diff objects only
+for diff_added in hcommit.diff(None):
+    print(diff_added)
+
+print(repo.tags)
 
 # status = repo.remotes.origin.status("")
 # print(status)
