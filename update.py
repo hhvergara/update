cd38d6e6f39940437ea483a764ef6ca230fc6c99
hhvergara <hhvservice@gmail.com>
2020-12-21 22:38:52 -0300

    otro commit

--- update.py
+++ update.py
@@ -1,13 +1,13 @@
 import git
 import os
 import logging
+
+git_path = f'{os.getcwd()}/update'
+
 logging.basicConfig(level=logging.INFO)
 GIT_PYTHON_TRACE = 'full'
 
-print (f'primera opción: {os.path.dirname(os.path.abspath(__file__))}')
-print(f'segunda opción: {os.getcwd()}')
-print (os.path.dirname(__file__))
-repo = git.Repo(f'{os.getcwd()}/update')
+repo = git.Repo(git_path)
 print (f'resultado del update: {repo.remotes.origin.pull("")}')
 
 # status = repo.remotes.origin.status("")
